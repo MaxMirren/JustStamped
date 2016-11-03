06a9f570cf80845e36d678a50198343252b86096
MaxMirren <maxmirrenga@gmail.com>
2016-11-04 00:41:31 +0300

    New class and connection to activity + changed gradle.build

--- app/src/main/java/com/maxm/just_stamped/tabs/sign_in_up/FirstMessageService.java
+++ app/src/main/java/com/maxm/just_stamped/tabs/sign_in_up/FirstMessageService.java
@@ -3,7 +3,9 @@ package com.maxm.just_stamped.tabs.sign_in_up;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.maxm.just_stamped.js.R;
 
@@ -13,13 +15,28 @@ import com.maxm.just_stamped.js.R;
 
 public class FirstMessageService extends AppCompatActivity {
 
-    Button btnGoogleAuthorization;
+    ImageButton btnGoogleAuthorization;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message_first_message);
+        setButtonGoogle();
     }
+
+    /*
+    Этот метод задает параметры слушателя кнопки для google_authorization и связывает ее с btnGoogleAuthorization
+    */
+    protected void setButtonGoogle () {
+        btnGoogleAuthorization = (ImageButton) findViewById(R.id.google_authorization);
+        btnGoogleAuthorization.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 
 
 }
